c3b03152fc31bdcec68eaeb36295ae21eabef3ad
timwis <tim@timwis.com>
2019-08-02 17:25:13 +0100

    Use execa instead of hologit in explode command

--- backend/commands/explode.js
+++ backend/commands/explode.js
@@ -2,7 +2,8 @@ const fs = require('fs')
 const csvParser = require('csv-parser')
 const TOML = require('@iarna/toml')
 const handlebars = require('handlebars')
-const { Repo, TreeObject } = require('hologit/lib')
+const execa = require('execa')
+const toReadableStream = require('to-readable-stream')
 
 exports.command = 'explode <file>'
 exports.desc = 'Create a TOML file for every row of a CSV <file>'
@@ -26,23 +27,29 @@ exports.explode = explode
 async function explode ({ fileStream, filenameTemplate }) {
   const renderFilename = handlebars.compile(filenameTemplate)
 
-  const repo = await Repo.getFromEnvironment()
-  const treeObject = new TreeObject(repo)
-
   return new Promise ((resolve, reject) => {
     const pendingWrites = []
 
     fileStream
       .pipe(csvParser())
-      .on('data', async (row) => {
-        const tomlRow = TOML.stringify(sortKeys(row))
+      .on('data', (row) => {
         const fileName = renderFilename(row)
+        const tomlRow = TOML.stringify(sortKeys(row))
+        const input = toReadableStream(tomlRow)
 
-        pendingWrites.push(treeObject.writeChild(fileName, tomlRow))
+        const subprocess = execa('git', ['hash-object', '-w', '--stdin'], { input })
+        pendingWrites.push(subprocess.then(({ stdout }) => {
+          return { fileName, hash: stdout }
+        }))
       })
       .on('end', async () => {
-        await Promise.all(pendingWrites)
-        resolve(treeObject.write()) // resolves to hash
+        const objects = await Promise.all(pendingWrites)
+        const tree = createTreeOfBlobs(objects)
+        const input = toReadableStream(tree)
+
+        const subprocess = execa('git', ['mktree'], { input })
+        const { stdout } = await subprocess
+        resolve(stdout)
       })
       .on('error', reject)
   })
@@ -57,4 +64,13 @@ function sortKeys (unsorted) {
     .forEach((key) => sorted[key] = unsorted[key])
 
   return sorted
+}
+
+function createTreeOfBlobs (objects) {
+  const mode = '100644'
+  const type = 'blob'
+  return objects
+    .filter((object) => object.hash.length > 0) // HOTFIX: Fixes race condition from captureOutputTrimmed
+    .map((object) => `${mode} ${type} ${object.hash}\t${object.fileName}`)
+    .join('\n') + '\n'
 }
