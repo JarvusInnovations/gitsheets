f34dcdff481fc1cc50935d5a51b45673d669c9e8
timwis <tim@timwis.com>
2019-08-05 19:58:10 +0100

    Add import endpoint and require ref on all endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const assert = require('http-assert')
 const git = require('git-client')
 const { explode, listRows } = require('./lib')
+
+const validRefPattern = /^[\w-]+$/
+const validFilenameTemplatePattern = /^[{}\w-\/]+$/
 
 module.exports = createServer
 
@@ -11,13 +15,36 @@ if (!module.parent) {
   })
 }
 
-async function createServer (gitRef) {
+async function createServer () {
   const app = new Koa()
   const router = new Router()
 
-  router.get('/', async (ctx) => {
-    const rows = await listRows(gitRef)
+  router.get('/:ref', async (ctx) => {
+    const ref = ctx.params.ref
+    assert(validRefPattern.test(ref), 400, 'invalid ref')
+    const rows = await listRows(ref)
     ctx.body = rows
+  })
+
+  router.post('/:ref/import', async (ctx) => {
+    const ref = ctx.params.ref
+    const fileStream = ctx.req
+    const filenameTemplate = ctx.request.query.filenameTemplate
+
+    assert(filenameTemplate, 400, 'missing filenameTemplate query param')
+    assert(ctx.request.type == 'text/csv', 400, 'content-type must be text/csv')
+    assert(validRefPattern.test(ref), 400, 'invalid ref')
+    assert(validFilenameTemplatePattern.test(filenameTemplate), 400, 'invalid filenameTemplate')
+
+    try {
+      const treeHash = await explode({ fileStream, filenameTemplate })
+      const commitHash = await git.commitTree(treeHash, { p: ref, m: 'import' })
+      const branchName = `pr-${Date.now()}`
+      await git.branch(branchName, commitHash)
+      ctx.body = { branch: branchName }
+    } catch (err) {
+      ctx.status = 500
+    }
   })
 
   return app
--- backend/test/server.spec.js
+++ backend/test/server.spec.js
@@ -1,22 +1,107 @@
 const request = require('supertest')
+const del = require('del')
+const fs = require('fs')
+const execa = require('execa')
+const git = require('git-client')
 const createServer = require('../server')
+const { explode } = require('../lib')
 
-const GIT_REF = 'fb070046498551bb49ce253ef13daddcb56949c1'
+const TEST_GIT_DIR = './test/.git'
+const SAMPLE_DATA = './test/fixtures/sample_data.csv'
+const SAMPLE_DATA_CHANGED = './test/fixtures/sample_data_changed.csv'
+const SAMPLE_DATA_ROW_COUNT = 10
 
 describe('server', () => {
   let server
 
-  beforeEach(async () => {
-    server = await createServer(GIT_REF)
+  beforeAll(() => {
+    process.env.GIT_DIR = TEST_GIT_DIR
   })
 
-  it('lists rows', async () => {
-    await request(server.callback())
-      .get('/')
+  beforeEach(async () => {
+    await setupRepo()
+    server = await createServer()
+  })
+
+  afterEach(async () => {
+    await teardownRepo()
+  })
+
+  test('lists rows', async () => {
+    await loadSampleData()
+
+    const response = await request(server.callback())
+      .get('/master')
       .expect(200)
-      .then((res) => {
-        expect(Array.isArray(res.body)).toBe(true)
-        expect(res.body.length).toBe(3)
-      })
+
+    expect(Array.isArray(response.body)).toBe(true)
+    expect(response.body.length).toBe(SAMPLE_DATA_ROW_COUNT)
+  })
+
+  describe('import', () => {
+    test('creates a proposal branch with exploded data', async () => {
+      const sampleData = fs.readFileSync(SAMPLE_DATA_CHANGED)
+
+      const response = await request(server.callback())
+        .post('/master/import?filenameTemplate={{id}}')
+        .type('csv')
+        .send(sampleData)
+        .expect(200)
+
+      expect(response.body).toHaveProperty('branch')
+      const branch = response.body.branch
+      expect(branch).toMatch('pr-')
+
+      const { stdout } = await execa('git', ['ls-tree', branch])
+      const lines = stdout.split('\n')
+      expect(lines.length).toBe(SAMPLE_DATA_ROW_COUNT)
+    })
+
+    test('omitting filenameTemplate throws an error', async () => {
+      const sampleData = fs.readFileSync(SAMPLE_DATA_CHANGED)
+
+      const response = await request(server.callback())
+        .post('/master/import')
+        .type('csv')
+        .send(sampleData)
+        .expect(400)
+    })
+
+    test('sending non-csv data throws an error', async () => {
+      const response = await request(server.callback())
+        .post('/master/import?filenameTemplate={{id}}')
+        .send({ foo: 'bar' })
+        .expect(400)
+    })
+
+    test.skip('omitting data throws an error', async () => {
+      const response = await request(server.callback())
+        .post('/master/import?filenameTemplate={{id}}')
+        .expect(400)
+    })
   })
 })
+
+async function setupRepo () {
+  await git.init()
+  await git.commit({
+    'allow-empty': true,
+    'allow-empty-message': true,
+    m: ''
+  })
+}
+
+async function teardownRepo () {
+  await del([TEST_GIT_DIR])
+}
+
+async function loadSampleData () {
+  const fileStream = fs.createReadStream(SAMPLE_DATA)
+  const filenameTemplate = '{{id}}'
+  const treeHash = await explode({ fileStream, filenameTemplate })
+  const commitHash = await git.commitTree(treeHash, {
+    p: 'HEAD',
+    m: 'sample data'
+  })
+  await git.updateRef('HEAD', commitHash)
+}
